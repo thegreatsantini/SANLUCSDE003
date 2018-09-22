8af12b22647221fe5f0ae0506ff518529cc50acd
thegreatsantini <lss13@my.fsu.edu>
2018-09-22 10:57:52 -0700

    added home component

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 // import AppliedRoute from './Components/AppliedRoute'
+import Home from './Components/Home';
 import Facebook from './Containers/Facebook';
 import Goodreads from './Containers/Goodreads';
-import Home from './Containers/Home';
 import NotFound from './Components/NotFound'
 
-export default ({}) => 
+export default () => 
         <Switch>
             <Route exact path='/' component={Home} />
             <Route path='/facebook' component={Facebook} />
