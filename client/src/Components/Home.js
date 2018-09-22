e62f2cf5eb400087a418b33a7a70fcaf18df9fb4
thegreatsantini <lss13@my.fsu.edu>
2018-09-22 10:58:24 -0700

    created Home component

---
+++ client/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="App">
+            <h1>
+                Thank you for your consideratino
+            </h1>
+        </div>
+    );
+}
