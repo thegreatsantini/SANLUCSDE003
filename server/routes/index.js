f7c059ccb1f7b3958c069e82e8a244f67ba85a6e
thegreatsantini <lss13@my.fsu.edu>
2018-09-22 15:12:59 -0700

    implemented server route to serve facebook posts to the front end

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const indexRoute = express.Router();
 const request = require('request');
+const cmd = require('node-cmd');
+const asyncExecCmd = require('async-exec-cmd');
+const fs = require('fs');
 
 indexRoute.get("/test", function (req, res) {
-    res.status(200).send({"item":"landing page"});
+    res.status(200).send({ "item": "landing page" });
 });
 
-indexRoute.get('/api/v1/facebook', function (req, res) {
-    
+indexRoute.get('/api/v1/facebook', async (req, response) => {
+    asyncExecCmd('python3 ./scripts/get_facebook_posts.py', function __cb(err, res, code, buffer) {
+        if (err) {
+            console.error(err, code)
+            return
+        }
+        const readData = fs.readFileSync('facebook_posts.txt', 'utf8', () => { return 'read populated file' })
+        const parsed = JSON.parse(readData)
+        response.send(parsed)
+    })
 })
 
 indexRoute.post('/api/v1/goodreads', function (req, res) {
-    
+
 })
 
 
