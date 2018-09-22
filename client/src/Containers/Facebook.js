5e5136044149904be6ecaae3d84ab3e4bb8de620
thegreatsantini <lss13@my.fsu.edu>
2018-09-22 15:03:56 -0700

    implemented FacebookPosts in parent facebook comp

--- client/src/Containers/Facebook.js
+++ client/src/Containers/Facebook.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import LoadingScreen from '../Components/LoadingScreen'
+import FacebookPosts from '../Components/FacebookPosts'
 
 export default class Facebook extends Component {
     constructor() {
         super()
         this.state = {
-
+            fetching: true,
+            posts: []
         }
     }
 
-    componentDidMount = () => {
-        console.log('Facebook', this.props)
+    componentDidMount = async () => {
+        const data = await fetch(`http://localhost:8080/api/v1/facebook`)
+        data.json().then( (posts)=> {
+            this.setState({
+                fetching: false,
+                posts
+            })
+        })
     }
 
     render() {
         return (
-            <div className="App">
-                <h1> Here's the Facebook Component </h1>
-            </div>
+            <React.Fragment className="App">
+                {
+                this.state.fetching 
+                    ? <LoadingScreen source='facebook' item='posts' />
+                    : <FacebookPosts data={this.state.posts} />
+                }
+            </React.Fragment>
         );
     }
 }
