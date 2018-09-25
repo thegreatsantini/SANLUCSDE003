99c7e9d18ea378ebbd5ce239c00c6c6f4fd6cac9
thegreatsantini <lss13@my.fsu.edu>
2018-09-24 18:56:18 -0700

    added logic for Oauth to server

--- server/server.js
+++ server/server.js
@@ -2,15 +2,28 @@ require('dotenv').config(); // loads the .env
 const express = require('express');
 const morgan = require('morgan');
 const indexRoute = require('./routes/index');
+const authRoute = require('./routes/auth');
 const cors = require('cors')
+const passportConfig = require('./passport');
+const session = require('express-session');
+
 // initialize app
 const app = express();
 
-app.use(morgan('tiny'))
+// middleware
+app.use(morgan('tiny'));
 app.use(cors());
+app.use(session({
+    secret: 'turple was here',
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passportConfig.initialize());
+app.use(passportConfig.session());
 
 // routes
-app.use('/', indexRoute)
+app.use('/', indexRoute);
+app.use('/auth', authRoute );
 
 
 const PORT = process.env.PORT || 8080
