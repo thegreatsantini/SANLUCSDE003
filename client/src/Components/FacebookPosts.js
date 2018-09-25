5859eda484ef167492ad44092d0299ae9d5d4325
thegreatsantini <lss13@my.fsu.edu>
2018-09-24 18:53:23 -0700

    added some css and js logic to help even out each panel size

--- client/src/Components/FacebookPosts.js
+++ client/src/Components/FacebookPosts.js
@@ -5,25 +5,38 @@ import {
     Row
 } from 'react-bootstrap'
 
+
 const containerStyle = {
-    padding: '15px 15%'
+    padding: '15px 15%',
+    display: 'flex',
+    // justifyContent: 'center',
+    flexWrap: 'wrap',
+    flex:'1 1 auto'
+}
+
+
+
+const panelStyle = {
+    // height: '50px'
+    // border: '15px solid black'
 }
 
 export default ({ data }) => {
 
     const UI = data.map((post, i) => {
+        const titleLength = (el) => el.length > 20 ? el.slice(0, el.length - 5) : el
         return (
-            <Col key={i} xs={6} md={4}>
-                <Panel>
-                    <Panel.Heading> {post.title} </Panel.Heading>
-                    <Panel.Body>{post.text}</Panel.Body>
+            <Col key={i} xs={12} sm={6} md={4}>
+                <Panel style={panelStyle} >
+                    <Panel.Heading> {titleLength(post.title)} </Panel.Heading>
+                    <Panel.Body> {post.text} </Panel.Body>
                     <Panel.Footer>{post.timestamp}</Panel.Footer>
                 </Panel>
             </Col>
         )
     })
     return (
-        <Row style={containerStyle}>
+        <Row style={containerStyle} >
             {UI}
         </Row>
     );
