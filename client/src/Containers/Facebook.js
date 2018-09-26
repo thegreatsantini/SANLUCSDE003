777ef5a79a606b97eef074a1b9e6d44caf6e9bd6
thegreatsantini <lss13@my.fsu.edu>
2018-09-25 20:45:54 -0700

    added data to localstorage

--- client/src/Containers/Facebook.js
+++ client/src/Containers/Facebook.js
@@ -12,22 +12,40 @@ export default class Facebook extends Component {
     }
 
     componentDidMount = async () => {
-        const data = await fetch(`http://localhost:8080/api/v1/facebook`)
-        data.json().then( (posts)=> {
+        this.fetchPosts()
+    }
+
+    fetchPosts = async () => {
+        const date = new Date();
+        const lastSearch = localStorage.getItem('lastSearch')
+        console.log(lastSearch, date.getHours())
+        if (localStorage.hasOwnProperty('posts') && lastSearch >= date.getHours()) {
+            const data = JSON.parse(localStorage.getItem('posts'))
             this.setState({
                 fetching: false,
-                posts
+                posts: data
             })
-        })
+        }
+        else {
+            const data = await fetch(`http://localhost:8080/api/v1/facebook`)
+            data.json().then((posts) => {
+                this.setState({
+                    fetching: false,
+                    posts
+                })
+                localStorage.setItem('posts', JSON.stringify(posts));
+                localStorage.setItem('lastSearch', date.getHours());
+            })
+        }
     }
 
     render() {
         return (
             <React.Fragment className="App">
                 {
-                this.state.fetching 
-                    ? <LoadingScreen source='facebook' item='posts' />
-                    : <FacebookPosts data={this.state.posts} />
+                    this.state.fetching
+                        ? <LoadingScreen source='facebook' item='posts' />
+                        : <FacebookPosts data={this.state.posts} />
                 }
             </React.Fragment>
         );
