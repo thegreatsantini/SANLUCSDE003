e4031b69f3c7540f8da74e562a14dda859202ad8
thegreatsantini <lss13@my.fsu.edu>
2018-09-25 20:46:18 -0700

    adding goodreads data to local storage

--- client/src/Containers/Goodreads.js
+++ client/src/Containers/Goodreads.js
@@ -6,39 +6,55 @@ import { Button } from 'react-bootstrap';
 const SERVER_URL = 'http://localhost:8080'
 
 export default class Goodreads extends Component {
-  constructor() {
-    super()
+    constructor() {
+        super()
         this.state = {
             fetching: true,
             quotes: []
         }
     }
 
-    componentDidMount = async() => {
+    componentDidMount = async () => {
         // const data = fetch(`${SERVER_URL}/auth/user`)
-       this.fetchQoutes()
+        this.fetchQoutes()
     }
-    
+
 
     fetchQoutes = async () => {
-        const data = await fetch(`http://localhost:8080/api/v1/goodreads`)
-        data.json().then( (quotes)=> {
+        const date = new Date();
+        const lastSearch = localStorage.getItem('lastSearch')
+        if (localStorage.hasOwnProperty('quotes') && lastSearch >= date.getHours()) {
+            const data = JSON.parse(localStorage.getItem('quotes'))
+            console.log(data)
             this.setState({
                 fetching: false,
-                quotes
+                quotes: data
             })
-        })
+        } 
+        else {
+            console.log('fetching')
+            const data = await fetch(`http://localhost:8080/api/v1/goodreads`)
+            data.json().then((quotes) => {
+                this.setState({
+                    fetching: false,
+                    quotes
+                })
+                console.log(quotes)
+                localStorage.setItem('quotes', JSON.stringify(quotes));
+                localStorage.setItem('lastSearch', date.getHours());
+            })
+        }
     }
 
     render() {
         return (
             <React.Fragment className="App">
-                {/* <Button href={ SERVER_URL + '/auth/login' }>Link</Button>   */}
+                {/* <Button href={ SERVER_URL + '/auth/login' }>Link</Button> */} 
                 {
-                this.state.fetching 
-                    ? <LoadingScreen source='goodreads' item='quotes' />
-                    : <GoodreadQuotes data={this.state.quotes} />
-                }
+                    this.state.fetching
+                        ? <LoadingScreen source='goodreads' item='quotes' />
+                        : <GoodreadQuotes data={this.state.quotes} />
+                } 
             </React.Fragment>
         );
     }
