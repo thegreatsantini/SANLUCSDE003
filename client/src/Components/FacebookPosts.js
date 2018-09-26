7b9e50da5f43484a6393640d784b72c70f865a24
thegreatsantini <lss13@my.fsu.edu>
2018-09-25 21:45:24 -0700

    added better resonsiveness and accomdates for long posts

--- client/src/Components/FacebookPosts.js
+++ client/src/Components/FacebookPosts.js
@@ -36,20 +36,30 @@ const panelStyle = {
 export default ({ data }) => {
 
     const UI = data.map((post, i) => {
-        // const titleLength = (el) => el.length > 20 ? el.slice(0, el.length - 8) + '...' : el
+        const titleLength = (el) => el.length > 45 ? el.slice(0, el.length - (8 + el.length)) + '...' : el;
+        const testLength = (el) => el.length > 145 ? el.slice(0, el.length - (8 + el.length)) + '...' : el;
         return (
             <Col key={i} xs={12} sm={6} md={4}>
                 <Panel style={panelStyle} >
-                    <Panel.Heading style={panelHeadingStyle}> {post.title} </Panel.Heading>
-                    <Panel.Body style={ panelBodyStyle }> {post.text} </Panel.Body>
+                    <Panel.Heading style={panelHeadingStyle}> {titleLength(post.title)} </Panel.Heading>
+                    <Panel.Body style={ panelBodyStyle }> {testLength(post.text)} </Panel.Body>
                     <Panel.Footer>{post.timestamp}</Panel.Footer>
                 </Panel>
             </Col>
         )
     })
+    const titleLength = (el) => el.length > 45 ? el.slice(0, el.length - (10 + el.length)) + '...' : el;
+const testLength = (el) => el.length > 145 ? el.slice(0, el.length - (10 + el.length)) + '...' : el;
     return (
         <Row bsClass='panel__container' style={containerStyle} >
             {UI}
+            <Col  xs={12} sm={6} md={4}>
+                <Panel style={panelStyle} >
+                    <Panel.Heading style={panelHeadingStyle}> {titleLength('this is an extemly long post tile. i am using it to test my cuntion please work correclty')} </Panel.Heading>
+                    <Panel.Body style={ panelBodyStyle }> {testLength('this is an extemly long post tst. i am using it to test my cuntion please work correclty. it should add a ... to the end of the post text in order to accomodate for long post lengths')} </Panel.Body>
+                    <Panel.Footer>'timestat</Panel.Footer>
+                </Panel>
+            </Col>
         </Row>
     );
 }
