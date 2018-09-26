3704062b83bb34cb0a077cfbc5905b7ffdab3ac5
thegreatsantini <lss13@my.fsu.edu>
2018-09-25 21:28:05 -0700

    better responsivness in facepost UI

--- client/src/Components/FacebookPosts.js
+++ client/src/Components/FacebookPosts.js
@@ -4,17 +4,29 @@ import {
     Panel,
     Row
 } from 'react-bootstrap'
-
+import './FacebookPosts.css'
 
 const containerStyle = {
-    padding: '15px 15%',
+    padding: '15px 10%',
     display: 'flex',
     // justifyContent: 'center',
     flexWrap: 'wrap',
     flex:'1 1 auto'
 }
 
+const panelBodyStyle = {
+    height: '135px',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+}
 
+const panelHeadingStyle = {
+    height: '60px',
+    justifyContent: 'center',
+    display: 'flex',
+    alignItems: 'center'
+}
 
 const panelStyle = {
     // height: '50px'
@@ -24,19 +36,19 @@ const panelStyle = {
 export default ({ data }) => {
 
     const UI = data.map((post, i) => {
-        const titleLength = (el) => el.length > 20 ? el.slice(0, el.length - 5) : el
+        // const titleLength = (el) => el.length > 20 ? el.slice(0, el.length - 8) + '...' : el
         return (
             <Col key={i} xs={12} sm={6} md={4}>
                 <Panel style={panelStyle} >
-                    <Panel.Heading> {titleLength(post.title)} </Panel.Heading>
-                    <Panel.Body> {post.text} </Panel.Body>
+                    <Panel.Heading style={panelHeadingStyle}> {post.title} </Panel.Heading>
+                    <Panel.Body style={ panelBodyStyle }> {post.text} </Panel.Body>
                     <Panel.Footer>{post.timestamp}</Panel.Footer>
                 </Panel>
             </Col>
         )
     })
     return (
-        <Row style={containerStyle} >
+        <Row bsClass='panel__container' style={containerStyle} >
             {UI}
         </Row>
     );
